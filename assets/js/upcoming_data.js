7a25a4b8bc072780ffa6387ff1f65e4f6d488791
Ricardo <gamaric70@yahoo.com>
2023-03-07 20:33:06 -0300

    agregado js contenido dinamico cards

---
+++ assets/js/upcoming_data.js
@@ -0,0 +1,29 @@
+let diaActual = data.currentDate;
+let eventos = data.events;
+const cardConatiner = document.querySelector(".card-contentido");
+let futuros = [];
+for (let index = 0; index < eventos.length; index++) {
+  if (eventos[index].date > diaActual) {
+    futuros.push(eventos[index]);
+  }
+}
+
+const cards = (array, contenedor) => {
+  let nodesCards = "";
+  for (let i = 0; i < array.length; i++) {
+    nodesCards += `<div class="card bg-success">
+    <img alt="${array[i].name}" src="${array[i].image}" class="card-img-top">
+    
+    <div class="card-body">
+    <h5 class="card-title">${array[i].name}</h5>
+    <p class="card-text">${array[i].description}</p>
+    <p class="card-precio">$${array[i].price}</p>
+    <a href="./details.html" class="btn btn-secondary hover">Datails</a>
+    </div>
+    
+    </div>
+    `;
+    contenedor.innerHTML = nodesCards;
+  }
+};
+cards(futuros, cardConatiner);
